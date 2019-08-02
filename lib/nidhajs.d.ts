d1a1d320881c803a64afbf3175430deb18b37194
sreenidha <kirkum80@gmail.com>
2019-08-03 00:44:49 +0800

    added drag behaviour methods

--- lib/nidhajs.d.ts
+++ lib/nidhajs.d.ts
@@ -1,24 +1,30 @@
 export declare abstract class LeafShape {
-    private x;
-    private y;
+    x: number;
+    y: number;
+    myroot: LeafShape;
     parentshape: LeafShape;
     baseshape: LeafShape;
     childshape: LeafShape[];
     x_adjust_factor: number;
     y_adjust_factor: number;
+    mynode: any;
+    iammoving: boolean;
     constructor(x: number, y: number, prntshape: any);
     abstract create(): void;
+    movingstart(): void;
+    movingend(x: number, y: number): void;
     moving(x: number, y: number): void;
     abstract moving_bare(): void;
     get_x(): number;
     get_y(): number;
     set_x(x: number): void;
     set_y(y: number): void;
-    private checkforempty;
+    isemptyobject(obj: any): boolean;
+    add_drag_forbase(): void;
+    set_myroot(): void;
 }
 export declare class base_circle_leaf extends LeafShape {
     radius: number;
-    mynode: any;
     constructor(x: number, y: number, prntshape: any, r: number);
     create(): void;
     moving_bare(): void;
@@ -26,8 +32,12 @@ export declare class base_circle_leaf extends LeafShape {
 export declare class base_rect_leaf extends LeafShape {
     width: number;
     height: number;
-    mynode: any;
     constructor(x: number, y: number, prntshape: any, w: number, h: number);
+    create(): void;
+    moving_bare(): void;
+}
+export declare class my_dot_leaf extends LeafShape {
+    constructor(x: number, y: number, prntshape: any, r: number);
     create(): void;
     moving_bare(): void;
 }
--- src/nidhajs.ts
+++ src/nidhajs.ts
@@ -2,22 +2,28 @@ import * as d3 from 'd3';
 
 export abstract class LeafShape {
 
-    private x: number;
-    private y: number;
+    public x: number;
+    public y: number;
+    public myroot : LeafShape;
     public parentshape : LeafShape;
     public baseshape : LeafShape;
     public childshape : LeafShape[];
     public x_adjust_factor: number;
     public y_adjust_factor: number;
+    public mynode: any;
+    public iammoving: boolean;
 
     constructor(x:number,y:number, prntshape:any) {
       this.x = 0;
       this.y = 0;
+      this.iammoving = false;
       this.x_adjust_factor = 0;
       this.y_adjust_factor = 0;
+      this.myroot = <LeafShape>{};
       this.childshape = [];
       this.parentshape = <LeafShape>{};
       this.baseshape = <LeafShape>{};
+      
 
       let nx = x;
       let ny = y;
@@ -30,6 +36,7 @@ export abstract class LeafShape {
       }
       this.set_x(nx);
       this.set_y(ny);
+      this.set_myroot();
     }
     
     ///////  CREATE SHAPES : start   ///////
@@ -38,17 +45,61 @@ export abstract class LeafShape {
     public abstract create():void;  
     ///////  CREATE SHAPES : end   ///////
 
+    movingstart() {
+      this.iammoving = true;
+    }
+
+    movingend(x:number,y:number) {
+      this.iammoving = false;
+    }
+
     public moving(x:number,y:number): void
     {
-          this.set_x(x);
-          this.set_y(y);
+         if(!this.isemptyobject(this.parentshape) && this.parentshape.baseshape === this && this.iammoving && this.isemptyobject(this.parentshape.parentshape)) {
+              // SCENARIO WHERE the moving element is the BASE of the ROOT
+              // I have parent (THAT IS THE LAST CHAIN OF PARENT in up direction) and i am the base shape for the parent and i am moving, so i am responsible to update parents x, y
+              // so all childs can allign with parent including me
+              // After updating parent with lateset x,y position i better allign with my parent
+             
+              this.parentshape.set_x(x);
+              this.parentshape.set_y(y);
+              this.set_x(x);
+              this.set_y(y);
 
-          if (this.baseshape === null && this.childshape=== null) {
+        } else if(!this.isemptyobject(this.parentshape) && this.parentshape.baseshape === this && this.iammoving && !this.isemptyobject(this.parentshape.parentshape)) {
+              // SCENARIO WHERE the moving element is the BASE of any SUB CLASS OF SUB ELEMENT of ROOT
+              // I have parent (THAT IS THE LAST CHAIN OF PARENT in up direction) and i am the base shape for the parent and i am moving, so i am responsible to update parents x, y
+              // so all childs can allign with parent including me
+              // After updating parent with lateset x,y position i better allign with my parent
+              this.parentshape.x = x;
+              this.parentshape.y = y;
+              this.set_x(x);
+              this.set_y(y);
+        
+        } else if (!this.isemptyobject(this.parentshape) && !this.iammoving) {
+              // SCENARIO WHERE the element is the CHILD of the moving BASE ELEMENT which of any SUB ELEMENT or BASE element of ROOT
+              // I have parent but i am moving because of some one, so i should not pollut parents x, y
+              // Since some one is moving me i better allign with my parent
+              this.set_x(x);
+              this.set_y(y);
+          }
+          else  if(!this.isemptyobject(this.parentshape) && this.parentshape.baseshape !== this && this.iammoving) {
+              // SCENARIO WHERE the moving element is the BASE of any SUB ELEMENT OF ROOT
+              // I have parent and i am NOT the base shape for the parent but i am moving, so i should not pollut parents x, y
+              // update my self only.  Since iam keeping me updated my childs will follow me
+              this.x = x;
+              this.y = y;
+        } 
+
+
+        if (this.isemptyobject(this.baseshape) && this.childshape.length===0) {
+
             this.moving_bare();
-          } 
-          if(this.childshape.length) {
-              this.childshape.forEach( le => { le.moving(0,0);})
-          }      
+            this.parentshape.childshape.forEach( le => { if(le!==this) {le.moving(this.get_x(),this.get_y())};});
+          } else {
+            this.baseshape.moving(this.get_x(),this.get_y());
+        } 
+          
     }
 
     public abstract moving_bare():void;
@@ -62,15 +113,14 @@ export abstract class LeafShape {
     }
 
     public set_x(x:number) {
-
-      this.checkforempty(this.parentshape) ? this.x = x: this.x = this.parentshape.get_x() + this.x_adjust_factor;
+      this.isemptyobject(this.parentshape) ? this.x = x : this.x = this.parentshape.get_x() + this.x_adjust_factor;
     }
 
     public set_y(y:number) {
-      this.checkforempty(this.parentshape)  ? this.y = y: this.y = this.parentshape.get_y() + this.y_adjust_factor;
+      this.isemptyobject(this.parentshape)  ? this.y = y: this.y = this.parentshape.get_y() + this.y_adjust_factor;
     }
 
-    private checkforempty(obj: any): boolean {
+    public isemptyobject(obj: any): boolean {
       if ((Object.keys(obj).length === 0 && obj.constructor === Object)) {
         return true;
       }  
@@ -78,12 +128,33 @@ export abstract class LeafShape {
 
     }
 
+   add_drag_forbase() {
+    // This adds the drag behaviour for the first BASE ELEMENT in the called object
+
+    if (typeof(this.mynode) === "undefined" && (!this.isemptyobject(this.baseshape))) {
+      this.baseshape.add_drag_forbase();
+    } else if (typeof(this.mynode) !== "undefined" && (this.isemptyobject(this.baseshape))) {
+        this.mynode
+                      //.attr("fill","yellow")
+                      //.raise()
+                        .call(d3.drag()
+                            .on("start", () => this.movingstart())
+                            .on("drag", () => this.moving(d3.event.x,d3.event.y))
+                            .on("end", () => this.movingend(d3.event.x,d3.event.y))
+                            );
+
+    }
+  }
+
+  set_myroot() {
+    this.isemptyobject(this.parentshape)  ? this.myroot = <LeafShape>{}: this.myroot = this.parentshape.myroot;
+  }
+
 }
 
 
 export class base_circle_leaf extends LeafShape {
   radius: number;
-  mynode: any;
 
   constructor(x:number,y:number, prntshape:any,r: number) {
     super(x,y,prntshape);
@@ -92,15 +163,16 @@ export class base_circle_leaf extends LeafShape {
   }
 
   create() {
-      this.mynode = d3.select("#nodelayer").append("circle")
+      this.mynode = d3.select("#leaflayer").append("circle")
                       .attr("r",this.radius)
                       .attr("cx",this.get_x())
                       .attr("cy",this.get_y());
   }
 
-  moving_bare() {
-    this.mynode.attr("cx",this.get_x())
-               .attr("cy",this.get_y());
+
+    moving_bare() {
+    this.mynode.attr("cx", this.get_x())
+               .attr("cy", this.get_y());
   }
 
 }
@@ -109,30 +181,52 @@ export class base_circle_leaf extends LeafShape {
 export class base_rect_leaf extends LeafShape {
   width: number;
   height: number;
-  mynode: any;
 
   constructor(x:number,y:number, prntshape:any,w: number,h: number) {
     super(x,y,prntshape);
     this.width = w;
     this.height = h;
+    this.myroot = this;
     this.create();
   }
 
   create() {
-      this.mynode = d3.select("#nodelayer").append("rect")
+      this.mynode = d3.select("#leaflayer")
+                      .append("rect")
                       .attr("width",this.width)
                       .attr("height",this.height)
                       .attr("x",this.get_x())
-                      .attr("y",this.get_y());
+                      .attr("y",this.get_y())
+                        ;
   }
 
   moving_bare() {
-    this.mynode.attr("x",this.get_x())
-               .attr("y",this.get_y());
+    this.mynode.attr("x", this.get_x())
+               .attr("y", this.get_y());
   }
-  
 
 }
+
+export class my_dot_leaf extends LeafShape {
+    constructor(x:number,y:number, prntshape:any,r: number) {
+    super(x,y,prntshape);
+    this.baseshape = new base_circle_leaf(0,0,this,r);
+    this.create();
+  }
+
+  create() {    
+    let msx= this.childshape.push(new base_circle_leaf(0,0,this,((<base_circle_leaf>this.baseshape).radius/2)));
+    console.log(this.childshape[msx-1].mynode);
+    this.childshape[msx-1].mynode.attr("fill","yellow");
+    console.log("sdddddd");
+    console.log(msx)
+  }
+
+  moving_bare() {
+  }
+
+}
+
 
 export class my_rect_leaf extends LeafShape {
     
@@ -143,9 +237,12 @@ export class my_rect_leaf extends LeafShape {
   }
 
   create() {
-    this.childshape.push(new base_circle_leaf(
+    //@TESTING CODES
+    this.childshape.push(new my_dot_leaf(             /* -->  this is test for NON BASE shape as child */
+    //this.childshape.push(new base_circle_leaf(      /* -->  this is test for BASE shape as child */
       ((<base_rect_leaf>this.baseshape).width),
-      ((<base_rect_leaf>this.baseshape).height)/2,this.baseshape,((<base_rect_leaf>this.baseshape).height/4)));
+      ((<base_rect_leaf>this.baseshape).height)/2,this,((<base_rect_leaf>this.baseshape).height/4)));
+    //@TESTING CODES
   }
 
   moving_bare() {
